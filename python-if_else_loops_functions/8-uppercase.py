c801b4f1a0804e4c4e0c7e4cb670a47aa3de1f4c
Emi-H106 <emi.hatano@yahoo.com>
2025-05-06 16:21:20 +0200

    add 8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    result = []
+
+    for char in str:
+        if 'a' <= char <= 'z':
+            upper_char = chr(ord(char) - 32)
+            result.append(upper_char)
+        else:
+            result.append(char)
+
+
+    print(''.join(result))
