0a590f7e64768673092446959c9075f02b692c3a
Emi-H106 <emi.hatano@yahoo.com>
2025-05-07 13:51:35 +0200

    fpllow bey print  functions with string format

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{0}{1}".format(i, j))
+        else:
+            print("{0}{1}".format(i, j), end=', ')
 
-print(
-    ', '.join(
-        f"{i}{j}"
-        for i in range(10)
-        for j in range(i + 1, 10)
-    ),
-    end='\n')
