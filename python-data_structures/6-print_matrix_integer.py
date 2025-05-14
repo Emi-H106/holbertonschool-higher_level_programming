4c42b877315a2f6b39136152451861bdb363fec2
Emi-H106 <emi.hatano@yahoo.com>
2025-05-14 09:36:30 +0200

    modify the contents of the format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
             if i == len(row) - 1:
-                print("{:d}".format(num), end="")
+                print("{:d}".format(row[i]), end="")
             else:
-                print("{:d}".format(num), end=" ")
+                print("{:d}".format(row[i]), end=" ")
         print()
