bef7067958bee8b31cbfdd2053181da78be10008
Emi-H106 <emi.hatano@yahoo.com>
2025-05-21 09:38:55 +0200

    add documentation for the module

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""
+This module defines a simple Square class
+"""
 
 class Square:
-    """Empty class Square that defines a square.
+    """
+    Empty class Square that defines a square.
     """
     pass
