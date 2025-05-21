f22fc747aeeb5f378be55254f94ce7d3f05783dc
Emi-H106 <emi.hatano@yahoo.com>
2025-05-21 13:00:17 +0200

    fix return for def __repr__

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -133,7 +133,7 @@ class Rectangle:
             str: The string representation of the rectangle.
         """
 
-        return " Rectangle({}, {})".format(self.__width, self.__height)
+        return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
         """
--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -143,7 +143,7 @@ class Rectangle:
             str: The string representation of the rectangle.
         """
 
-        return " Rectangle({}, {})".format(self.__width, self.__height)
+        return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
         """
