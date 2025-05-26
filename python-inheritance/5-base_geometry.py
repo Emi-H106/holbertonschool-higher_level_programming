18b4cccfa0ae7a2c547146d40566c83c803af41d
Emi-H106 <emi.hatano@yahoo.com>
2025-05-26 16:21:13 +0200

    add 5-base_geometry.py

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module for a base class for geometry-related operations.
+"""
+
+
+class BaseGeometry:
+    """
+    A base class for geometry-related operations.
+    """
+    pass
