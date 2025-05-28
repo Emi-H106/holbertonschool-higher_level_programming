0798d5d95365dd9e1177461ac99bf8e55b4276f3
Emi-H106 <emi.hatano@yahoo.com>
2025-05-28 14:00:10 +0200

    add documentation for Returns

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -10,6 +10,10 @@ def inherits_from(obj, a_class):
     """
     Check if the object is an instance of a class that
     inherited (directly or indirectly) from the specified class.
+
+    Returns:
+        True: if the object is an instance of a class that inherited (directly or indirectly) from the specified class
+        False: otherwise
     """
     if isinstance(obj, a_class):
         if  type(obj) is not a_class:
