84cd9da48dfa19925338161d7b2d5a1e4e303bfd
Emi-H106 <emi.hatano@yahoo.com>
2025-05-28 17:08:56 +0200

    fix initialization

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -90,8 +90,8 @@ class Square(Rectangle):
         """
 
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
+        self.__size = size
 
     def area(self):
         """
