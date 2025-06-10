ac6173d10ff3cf4c8c8606296291f66ac13a59c2
Emi-H106 <emi.hatano@yahoo.com>
2025-06-10 12:53:05 +0200

    add documentation

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python3
-
+"""
+This module provides functions to fetch post data from the JSONPlaceholder API,
+print their titles, and save them to a CSV file.
+"""
 
 import requests
 import csv
 
 
 def fetch_and_print_posts():
+    """
+    Fetches post data from JSONPlaceholder and prints
+    the title of each post.
+    """
     response = requests.get('https://jsonplaceholder.typicode.com/posts')
-    print("Status Code:{}".format(response.status_code))
+    print(f"Status Code: {response.status_code}")
     if response.status_code == 200:
           posts = response.json()
           for post in posts:
             print(post['title'])
 
 def fetch_and_save_posts():
+    """
+    Fetches post data from JSONPlaceholder and saves it to
+    a CSV file named 'posts.csv', including the post's
+    id, title, and body.
+    """
     response = requests.get('https://jsonplaceholder.typicode.com/posts')
     if response.status_code == 200:
           posts = response.json()
