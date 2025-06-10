67d2bbd8b4967ff5a704a7115e29f24f25238657
Emi-H106 <emi.hatano@yahoo.com>
2025-06-10 13:45:31 +0200

    add tsk_03

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+
+class SimpleAPIHandler(BaseHTTPRequestHandler):
+    """
+     A simple HTTP request handler class for
+     demonstrating basic API functionality.
+    """
+    def do_GET(self):
+        """
+        Handle GET requests.
+        This method checks the request path and serves appropriate responses.
+        """
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode())
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+        elif self.path == '/info':
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            info = {
+                    "version": "1.0",
+                    "description": "A simple API built with http.server"
+                    }
+            self.wfile.write(json.dumps(info).encode())
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            error = {"error": "Endpoint not found"}
+            self.wfile.write(json.dumps(error).encode())
+
+
+def run(server_class=HTTPServer, handler_class=SimpleAPIHandler, port=8000):
+    """
+    Run the HTTP server
+    This function initializes and starts
+    the HTTP server on the specified port.
+    """
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd server on port {port}...')
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
