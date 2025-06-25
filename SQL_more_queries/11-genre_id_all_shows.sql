b97b17fd0f231aa526c6f4cc8f06b8fe595e5a8f
Emi-H106 <emi.hatano@yahoo.com>
2025-06-25 09:38:44 +0200

    add genre_id_all_shows

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- Script that lists all shows contained in the database hbtn_0d_tvshows
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- If a show doesn’t have a genre, display NUL
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres on  tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
