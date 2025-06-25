2fd204b0d873b845bb03e6adc346d968f905a9c4
Emi-H106 <emi.hatano@yahoo.com>
2025-06-25 16:04:53 +0200

    add select_states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module is used to connect to the database mysqldb.
+"""
+
+import MySQLdb
+import sys
+
+
+def list_states(username, password, database):
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_states(username, password, database)
