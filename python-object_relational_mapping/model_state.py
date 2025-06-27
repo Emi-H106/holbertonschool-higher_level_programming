e64764682c5e886386bd17732f6e360291e2275a
Emi-H106 <emi.hatano@yahoo.com>
2025-06-27 10:57:07 +0200

    fix pycodestyle

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """
     The State class corresponds to the states table in the database
