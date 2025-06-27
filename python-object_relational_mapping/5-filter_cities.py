dfc3d0690539c20095d3831120197bc29f69de7f
Emi-H106 <emi.hatano@yahoo.com>
2025-06-27 09:20:31 +0200

    add filter_cities

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+This script retrieves and displays all cities from the database
+that match the specified state name
+"""
+
+
+import MySQLdb
+import sys
+
+
+def list_cities_by_states(username, password, database, state_name):
+    """
+    Retrieve all cities that match the specified state name and
+    display them in ascending order by ID.
+    """
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+
+    cursor = db.cursor()
+    query = """
+    SELECT cities.name FROM cities
+    JOIN states on cities.state_id = states.id
+    WHERE states.name = %s ORDER BY cities.id ASC
+    """
+    cursor.execute(query, (state_name,))
+    results = cursor.fetchall()
+    cities_list = [row[0] for row in results]
+    cities_list_result = ", ".join(cities_list)
+    print(cities_list_result)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    list_cities_by_states(username, password, database, state_name)
