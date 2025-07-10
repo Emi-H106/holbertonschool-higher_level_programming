44065a2a1de1b519c885b9c7e29df00a4a371973
Emi-H106 <emi.hatano@yahoo.com>
2025-07-10 18:27:04 +0200

    add 7-multi_c.js

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+const count = parseInt(arg);
+
+if (isNaN(count) || count <= 0) {
+  if (isNaN(count)) {
+    console.log('Missing number of occurrences');
+    }
+
+} else {
+  let i = 0;
+  while (i < count) {
+    console.log('C is fun');
+    i++;
+     }
+  }
