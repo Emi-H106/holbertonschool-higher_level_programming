2ac69557944a55a035bc43f6fdd4fbe44485e159
Emi-H106 <emi.hatano@yahoo.com>
2025-07-10 18:32:26 +0200

    fix indent

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -6,12 +6,11 @@ const count = parseInt(arg);
 if (isNaN(count) || count <= 0) {
   if (isNaN(count)) {
     console.log('Missing number of occurrences');
-    }
-
+  }
 } else {
   let i = 0;
   while (i < count) {
     console.log('C is fun');
     i++;
-     }
   }
+}
